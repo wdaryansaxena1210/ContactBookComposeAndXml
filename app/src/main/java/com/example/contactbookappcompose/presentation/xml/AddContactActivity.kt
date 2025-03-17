7c3ac0e25072ef5eb54cfa44e44cbff5dfd9c8f3
Aryan Saxena <aryansaxena1210@gmail.com>
2025-03-17 16:14:58 -0500

    Add add-contact Screen

---
+++ app/src/main/java/com/example/contactbookappcompose/presentation/xml/AddContactActivity.kt
@@ -0,0 +1,55 @@
+package com.example.contactbookappcompose.presentation.xml
+
+import android.os.Bundle
+import android.text.Editable
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.contactbookappcompose.R
+import com.example.contactbookappcompose.data.local.Contact
+import com.example.contactbookappcompose.domain.contact.ContactData
+import com.example.contactbookappcompose.presentation.compose.ContactViewModel
+
+class AddContactActivity : AppCompatActivity() {
+
+    private  val viewModel : ContactViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_add_contact)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val firstNameEditText = findViewById<EditText>(R.id.etFirstName)
+        val lastNameEditText = findViewById<EditText>(R.id.etLastName)
+        val phoneNumberEditText = findViewById<EditText>(R.id.etPhoneNumber)
+        val emailEditText = findViewById<EditText>(R.id.etEmail)
+        val addressEditText = findViewById<EditText>(R.id.etAddress)
+        val companyNameEditText = findViewById<EditText>(R.id.etCompanyName)
+        val saveButton = findViewById<Button>(R.id.btnSaveContact)
+
+        saveButton.setOnClickListener{
+            val contact = ContactData(
+                firstName = firstNameEditText.text.toString(),
+                lastName = lastNameEditText.text.toString(),
+                companyName = companyNameEditText.text.toString(),
+                email = emailEditText.text.toString(),
+                address = addressEditText.text.toString(),
+                phoneNumber = phoneNumberEditText.text.toString()
+            )
+
+            viewModel.addContact(contact)
+
+            finish()
+        }
+
+    }
+}
--- app/src/main/java/com/example/contactbookappcompose/presentation/xml/ContactListActivity.kt
+++ app/src/main/java/com/example/contactbookappcompose/presentation/xml/ContactListActivity.kt
@@ -1,6 +1,8 @@
 package com.example.contactbookappcompose.presentation.xml
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -12,11 +14,13 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.contactbookappcompose.R
 import com.example.contactbookappcompose.presentation.compose.ContactViewModel
 import com.example.contactbookappcompose.presentation.xml.adapter.ContactListAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ContactListActivity : AppCompatActivity() {
 
     lateinit var contactRecyclerView: RecyclerView
     private val viewModel : ContactViewModel by viewModels()
+    lateinit var addContactButton : FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +31,13 @@ class ContactListActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        addContactButton = findViewById(R.id.btnAddContact)
+        addContactButton.setOnClickListener {
+            val intent = Intent(this, AddContactActivity::class.java)
+            startActivity(intent)
+        }
+
 
         contactRecyclerView = findViewById(R.id.contactRecyclerView)
         contactRecyclerView.layoutManager = LinearLayoutManager(this@ContactListActivity)
@@ -39,6 +50,5 @@ class ContactListActivity : AppCompatActivity() {
                 contactRecyclerView.adapter = ContactListAdapter(contacts = state.contacts, onClick = { println(it.id) })
             }
         }
-
     }
 }
